47f2674f689fcc395183839ed82877abdeee7c8d
Arthur Forgeard <arforgea@1H2.42angouleme.fr>
2023-01-25 06:53:07 +0100

    put_sprite

--- src/data.c
+++ src/data.c
@@ -1,4 +1,4 @@
-#include "cub3D.h"
+#include "../include/cub3D.h"
 
 static void	set_hook(t_data *data)
 {
--- src/hook.c
+++ src/hook.c
@@ -1,10 +1,30 @@
-#include "cub3D.h"
+#include "../include/cub3D.h"
 
 int	key_hook(int keycode, t_data *data)
 {
 	if (keycode == 65307)
 		mlx_loop_end(data->mlx);
 	return (0);
+}
+
+void	put_sprite(t_data *data, int pos_y, int pos_x, t_img sprite)
+{
+	int y = 0;
+	int x = 0;
+	int index = 0;
+
+	while (y < sprite.height)
+	{
+			x = 0;
+			while (x < sprite.width)
+			{
+				if (((int*)sprite.buf)[index] != COLOR_MASK)
+					((int*)data->img.buf)[(pos_y + y) * data->img.width + (pos_x + x)] = ((int*)sprite.buf)[index];
+				index++;
+				x++;
+			}
+			y++;
+	}
 }
 
 int	render_next_frame(t_data *data)
